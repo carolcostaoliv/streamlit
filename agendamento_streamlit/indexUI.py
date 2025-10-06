00b9cf650016988380f51407be5d0a9c918b3d15
carolcostaoliv <carolinaoliveiraacosta@gmail.com>
2025-10-06 13:02:36 +0000

    terminei atv11

--- agendamento_streamlit/indexUI.py
+++ agendamento_streamlit/indexUI.py
@@ -2,6 +2,10 @@ from templates.ClienteUI import ManterClienteUI
 from templates.ManterServicoUI import ManterServicoUI
 from templates.ManterHorarioUI import ManterHorarioUI
 from templates.ManterProfissionalUI import ManterProfissionalUI
+from templates.abrircontaUI import AbrirContaUI
+from templates.loginUI import LoginUI
+from templates.perfilclienteUI import PerfilClienteUI
+from view import View
 import streamlit as st
 
 class IndexUI:
@@ -13,10 +17,42 @@ class IndexUI:
         if op == "Cadastro de Horários": ManterHorarioUI.main()
         if op == "Cadastro de Profissionais": ManterProfissionalUI.main()
 
+    def menu_visitante():
+        op = st.sidebar.selectbox("Menu", ["Entrar no Sistema", "Abrir Conta"])
+        if op == "Entrar no Sistema": LoginUI.main()
+        if op == "Abrir Conta": AbrirContaUI.main()
+
+    def menu_cliente():
+        op = st.sidebar.selectbox("Menu", ["Meus Dados"])
+        if op == "Meus Dados": PerfilClienteUI.main()
+
+    def menu_admin():
+        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes", "Cadastro de Serviços", "Cadastro de Horários", "Cadastro de Profissionais"])
+        if op == "Cadastro de Clientes": ManterClienteUI.main()
+        if op == "Cadastro de Serviços": ManterServicoUI.main()
+        if op == "Cadastro de Horários": ManterHorarioUI.main()
+        if op == "Cadastro de Profissionais": ManterProfissionalUI.main()
+
+    def sair_do_sistema():
+        if st.sidebar.button("Sair"):
+            del st.session_state["usuario_id"]
+            del st.session_state["usuario_nome"]
+            st.rerun()
+
     def sidebar():
-        IndexUI.menu_admin()
+        if "usuario_id" not in st.session_state:
+            IndexUI.menu_visitante()
+        else:
+            admin = st.session_state["usuario_nome"] == "admin"
+            st.sidebar.write("Bem-vindo(a), " + st.session_state["usuario_nome"])
+            if admin: 
+                IndexUI.menu_admin()
+            else: 
+                IndexUI.menu_cliente()
+            IndexUI.sair_do_sistema()
 
     def main():
+        View.cliente_criar_admin()
         IndexUI.sidebar()
 
 IndexUI.main()
--- agendamento_streamlit/models/Cliente.py
+++ agendamento_streamlit/models/Cliente.py
@@ -2,11 +2,15 @@ import json
 
 class Cliente:
 
-    def __init__(self, id, nome, email, fone):
+    def __init__(self, id, nome, email, fone, senha):
         self.__id = id
         self.__nome = nome
         self.__email = email
         self.__fone = fone
+        self.set_senha(senha)
+
+    def get_senha(self): return self.__senha
+    def set_senha(self, senha): self.__senha = senha
 
     def set_id(self, id):
         if id < 0: raise ValueError ("ID inválido")
@@ -37,12 +41,12 @@ class Cliente:
         return f"{self.__id} - {self.__nome} | {self.__email} | {self.__fone}"
 
     def to_json(self):
-        dic = {"id":self.__id, "nome":self.__nome,"email":self.__email, "fone":self.__fone}
+        dic = {"id":self.__id, "nome":self.__nome,"email":self.__email, "fone":self.__fone, "senha": self.__senha}
         return dic
 
     @staticmethod
     def from_json(dic):
-        return Cliente(dic["id"], dic["nome"], dic["email"], dic["fone"])
+        return Cliente(dic["id"], dic["nome"], dic["email"], dic["fone"], dic["senha"])
 
 import json
 
